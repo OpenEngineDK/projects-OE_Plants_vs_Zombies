1acf55372bdab63a9bf697b2d527230b512beefd
skeen <skeen@cs.au.dk>
2011-07-20 23:26:57 +0000

    added some forgotten files

---
+++ Players/PassivePlayer.h
@@ -0,0 +1,23 @@
+#ifndef _OE_CHESS_PASSIVE_PLAYER_H_
+#define _OE_CHESS_PASSIVE_PLAYER_H_
+
+#include "../Player.h"
+
+class PassivePlayer : public Player
+{
+    //---------
+    //Functions
+    public:
+        void update()
+        {
+        }
+    protected:    
+    //---------
+    //Variables
+    public:
+    private:
+    protected:
+};
+
+#endif //_OE_CHESS_PASSIVE_PLAYER_H_
+
